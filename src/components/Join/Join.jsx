37241ac1dde04b17776b9cae36e2c33d834e3289
Christopher Ivan <my.subscription2001@gmail.com>
2020-08-18 00:39:19 +0700

    frontend done

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Join.css";
+
+const Join = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">
+          PPAB UKT <br /> Private Chatroom
+        </h1>
+        <div>
+          <input
+            type="text"
+            placeholder="username"
+            className="joinInput"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="room"
+            className="joinInput mt-20"
+            onChange={(e) => {
+              setRoom(e.target.value);
+            }}
+          />
+        </div>
+        <Link
+          onClick={(e) => {
+            if (!name || !room) {
+              e.preventDefault();
+            }
+          }}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20" type="submit">
+            Enter
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
---
+++ src/components/TextContainer/TextContainer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import onlineIcon from "../../icons/onlineIcon.png";
+
+import "./TextContainer.css";
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    {users ? (
+      <div>
+        <div className="textContainerOnline">
+          <h3>People online:</h3>
+        </div>
+        <div className="activeContainer">
+          <p>
+            {users.map(({ name }) => (
+              <div key={name} className="activeItem">
+                {name}
+                <img alt="Online Icon" src={onlineIcon} />
+              </div>
+            ))}
+          </p>
+        </div>
+      </div>
+    ) : null}
+  </div>
+);
+
+export default TextContainer;
